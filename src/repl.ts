99b749a4d31da7b85235411c5eada25d78b53c47
AhmadT14 <ahragh@gmail.com>
2026-02-11 10:49:25 +0200

    Add REPL utilities

---
+++ src/repl.ts
@@ -0,0 +1,26 @@
+function cleanInput(str: string): string[]{return str.toLowerCase().trim().split(/\s+/)}
+import { State } from './state';
+
+async function startREPL(initState: () => State) {
+  const state = initState();
+  const { rl, CLICommands } = state;
+    rl.prompt();
+    rl.on('line', async (line) => {
+  const words = cleanInput(line);
+  if(words.length === 0) {
+    rl.prompt();
+    return;
+  }
+    const commands = CLICommands;
+    const commandName = words[0];
+    const command = commands[commandName];
+    if (command) {
+      await command.callback(state);
+    } else {
+      console.log(`Unknown command: ${commandName}`);
+    }
+    rl.prompt();
+});
+}
+
+export {cleanInput,startREPL}
